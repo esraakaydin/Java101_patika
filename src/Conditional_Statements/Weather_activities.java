07039e856a9651cd391e26cf946e9c1abe6c79d7
esraakaydin <akaydinesraa@gmail.com>
2024-03-19 02:20:56 +0300

    Activity suggestions according to the weather

--- src/Conditional_Statements/Weather_activities.java
+++ src/Conditional_Statements/Weather_activities.java
@@ -1,5 +1,7 @@
 package Conditional_Statements;
+
 import java.util.Scanner;
+
 public class Weather_activities {
     public static void main(String[] args) {
 
@@ -8,21 +10,17 @@ public class Weather_activities {
         Scanner input = new Scanner(System.in);
         heat = input.nextInt();
 
-        if (heat<5)
-        {
+        if (heat < 5) {
             System.out.println("Skiing in this weather can be fun!");
-        }
-        else if ( heat >=5 && heat <= 15)
-        {
-            System.out.println("Going to the movies might be fun in this weather.");
-        }
-        else if (heat >15 && heat <=25)
-        {
-            System.out.println("Going on a picnic in this weather can be fun.");
-        }
-        else if(heat >25){
+        } else if (heat >= 5 && heat <= 25) {
+            if (heat <= 15) {
+                System.out.println("Going to the movies might be fun in this weather.");
+            }
+            if (heat >= 10) {
+                System.out.println("Going on a picnic in this weather can be fun.");
+            }
+        } else {
             System.out.println("Swimming in this weather can be fun.");
         }
-
     }
 }
