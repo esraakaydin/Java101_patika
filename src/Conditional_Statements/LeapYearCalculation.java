2e1f5d474000c1f45c1e5157f75817276d3850da
esraakaydin <akaydinesraa@gmail.com>
2024-05-11 18:02:45 +0300

    Leap Year Calculation

--- src/Conditional_Statements/LeapYearCalculation.java
+++ src/Conditional_Statements/LeapYearCalculation.java
@@ -12,20 +12,17 @@ public class LeapYearCalculation {
         System.out.println("Enter the year you want to find out if it is a leap year : ");
         year = input.nextInt();
 
-        remain = year % 4;
-        remain2 = year % 100;
-        remain3 = year % 400;
 
-        if (remain2 == 0) {
-            if (remain == 0 && remain3 == 0) {
+        if (year % 100 == 0) {
+            if (year % 4 == 0 && year % 400 == 0) {
                 System.out.println("This year is a leap year. ");
             } else {
                 System.out.println("This year isnt a leap year. ");
             }
 
-        } else if (remain == 0 && remain2 != 0) {
+        } else if (year % 4 == 0 && year % 100 != 0) {
             System.out.println("This year is a leap year. ");
-        } else if (remain != 0) {
+        } else if (year % 4!= 0) {
             System.out.println("This year isnt a leap year. ");
         }
 
