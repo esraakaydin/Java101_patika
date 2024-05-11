7e5bf34dc840bb46ed4aeec72a790bdbfae34f77
esraakaydin <akaydinesraa@gmail.com>
2024-05-12 02:36:07 +0300

    Training

--- src/Loops/training.java
+++ src/Loops/training.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class training {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int i=1;
-       while ( i<=10){
-           i++;
-           if ( i %2 == 0){
-               continue;
-           }
-           System.out.println(i);
-       }
+        int i = 1;
+        while (i <= 10) {
+            i++;
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
 
 
     }
