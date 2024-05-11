d7198a0a4f3c8f2e0d575671caed77d48c68be86
esraakaydin <akaydinesraa@gmail.com>
2024-05-12 02:33:20 +0300

    Average Calculation

---
+++ src/Loops/avregaeCalculation.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class avregaeCalculation {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int num, num1, total = 0, count = 0;
+        double average;
+        System.out.println("Enter the number : ");
+        num = input.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                System.out.println(i);
+                total += i;
+                count++;
+            }
+        }
+        average = total / count;
+        System.out.println("Average of numbers that are multiples of 3 and 4, from 0 to the entered number : " + average );
+    }
+}
--- src/Loops/training.java
+++ src/Loops/training.java
@@ -5,20 +5,15 @@ import java.util.Scanner;
 public class training {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int i=1;
+       while ( i<=10){
+           i++;
+           if ( i %2 == 0){
+               continue;
+           }
+           System.out.println(i);
+       }
 
-        int number ;
-        /* for ( boolean run = true ; run ; ){
-            System.out.println("Enter the number  : ");
-            number = input.nextInt();
-            if(number<0){
-                run = false ;
-            }
-        } */
 
-        do {
-            System.out.println("Enter the number : ");
-            number = input.nextInt();
-        }
-        while (number >0);
     }
 }
